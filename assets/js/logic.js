ac33d596fffef144e2b702043ca5faa9cabdbaec
damnsemicolon <shannoninengland@gmail.com>
2022-12-04 22:58:55 +0000

    kept the code dry and deleted duplicate folders

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // DOM set up
 var startScreen = document.querySelector("#start-screen");
 var startButton = document.querySelector("#start");
@@ -23,37 +22,6 @@ var numberOfQuestions = questionBook.length;
 
 // Set up function to start the quiz
 function showQuestion(question) {
-=======
-// Create a code quiz that contains the following requirements:
-var numberOfQuestions = listOfQuestions.length;
-var currentQuestion = 0;
-var pointsScored = 0;
-var pointsToAdd = 10;
-
-// Variables to get DOM elements needed
-var questionTitle = document.querySelector("#question-title");
-var choices = document.querySelector("#choices");
-var startScreen = document.querySelector("#start-screen");
-var questions = document.querySelector("#questions");
-var endScreen = document.querySelector("#end-screen");
-var finalScore = document.querySelector("#final-score");
-var submitBtn = document.querySelector("#submit");
-var feedback = document.querySelector("#feedback");
-
-// * A start button that when clicked a timer starts and the first question appears.
-var startButton = document.querySelector("#start");
-var time = document.querySelector("#time");
-var timerStarted = false;
-var timeCountdown = 60;
-var timePenalty = 10;
-
-//===========//
-// FUNCTIONS //
-//===========//
-
-// Logic to "feed" questions into the right area
-function addQuestion(question) { // Add question text
->>>>>>> 9970b7c7e939a89c428bd22465a84de6d0c9d442
     questionTitle.textContent = question[0];
 
     //Clear previous question choices and show next question choices
@@ -62,11 +30,7 @@ function addQuestion(question) { // Add question text
     }
 
     // Add list structure with possible answers
-<<<<<<< HEAD
     var ul = document.createElement("ul");
-=======
-    var ulEl = document.createElement("ul");
->>>>>>> 9970b7c7e939a89c428bd22465a84de6d0c9d442
     var choicesArr = question[1];
     var rightChoice = question[2];
 
@@ -81,7 +45,6 @@ function addQuestion(question) { // Add question text
         var btn = document.createElement("button");
         btn.textContent = (i + 1) + ". " + choicesArr[i];
         liEl.appendChild(btn);
-<<<<<<< HEAD
         ul.appendChild(liEl);
         ul.add;
         choices.appendChild(ul);
@@ -113,14 +76,6 @@ startButton.addEventListener("click", function (event) {
 
 // Set up function to show questions
 
-=======
-        ulEl.appendChild(liEl);
-        ulEl.add;
-        choices.appendChild(ulEl);
-    }
-    choices.add;
-}
->>>>>>> 9970b7c7e939a89c428bd22465a84de6d0c9d442
 
 function endGame() { // * When the game ends, it should display their score and give the user the ability to save their initials and their score
     // Set timer to 0
@@ -132,44 +87,12 @@ function endGame() { // * When the game ends, it should display their score and 
     // Hide questions and show end screen
     questions.setAttribute("class", "hide");
     endScreen.setAttribute("class", "start");
-<<<<<<< HEAD
-=======
-
->>>>>>> 9970b7c7e939a89c428bd22465a84de6d0c9d442
 }
 
 //===========//
 // LISTENERS //
 //===========//
 
-<<<<<<< HEAD
-
-=======
-startButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    // Add questions to the page
-    addQuestion(listOfQuestions[currentQuestion]);
-
-    // Hide start screen and show questions
-    startScreen.setAttribute("class", "hide");
-    questions.setAttribute("class", "show");
-
-    // Start the timer
-    if (! timerStarted) {
-        timerStarted = true;
-        var myTimer = setInterval(function () {
-            if (timeCountdown <= 0) { 
-                // * The quiz should end when the timer reaches 0
-                clearInterval(myTimer);
-                endGame();
-            } else {
-                timeCountdown--;
-            }
-            time.textContent = timeCountdown;
-        }, 1000)
-    }
-});
->>>>>>> 9970b7c7e939a89c428bd22465a84de6d0c9d442
 
 // * When answer is clicked, the next question appears
 // Put a listener for the questions area
@@ -192,19 +115,11 @@ questions.addEventListener("click", function (event) {
             if (currentQuestion === numberOfQuestions) { // * The quiz should end when all questions are answered.
                 endGame(); 
             } else { // Add next question
-<<<<<<< HEAD
                 showQuestion(questionBook[currentQuestion]);
             }
         } else { 
             // * If the answer clicked was incorrect then subtract time from the clock
             timeCountdown = timeCountdown - wrongPenalty;
-=======
-                addQuestion(listOfQuestions[currentQuestion]);
-            }
-        } else { 
-            // * If the answer clicked was incorrect then subtract time from the clock
-            timeCountdown = timeCountdown - timePenalty;
->>>>>>> 9970b7c7e939a89c428bd22465a84de6d0c9d442
 
             // Provide feedback to wrong answer
             var audioWrong = new Audio("./assets/sfx/incorrect.wav");
@@ -216,11 +131,7 @@ questions.addEventListener("click", function (event) {
     }
 })
 
-<<<<<<< HEAD
 submitButton.addEventListener("click", function() {
-=======
-submitBtn.addEventListener("click", function() {
->>>>>>> 9970b7c7e939a89c428bd22465a84de6d0c9d442
 
     // Create array to hold the score and retrieve any eventual scores already stored in the localStorage
     var scoreArr = JSON.parse(localStorage.getItem('score')) || [];
